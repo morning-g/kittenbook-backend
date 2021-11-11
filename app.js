2388535bfcabe025837e4a7504d60343911eae23
cattybog <72874739+cattybog@users.noreply.github.com>
2021-11-10 23:18:03 -0600

    Ya funcionan llamadas a la API

--- app.js
+++ app.js
@@ -11,52 +11,64 @@ const mysql2 = require("mysql2");
 // Se crea la app
 const app = express();
 // Puerto del servidor local
-const port = 3005;
-
-// Importar los archivos de las rutas
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/api/usuarios");
-// Se asignan las rutas del servidor a los archivos correspondientes en la carpeta routes
-app.use("/", indexRouter);
-app.use("/api/usuarios", usersRouter);
+const port = process.env.PORT || 3005;
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 app.use(logger("dev"));
-app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+    next();
+});
+const corsOptions = {
+    origin: '*',
+    credentials: true,
+    optionSuccessStatus: 200
+};
+app.use(cors(corsOptions));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+// Importar los archivos de las rutas
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/api/usuarios");
+
+// Se asignan las rutas del servidor a los archivos correspondientes en la carpeta routes
+app.use("/", indexRouter);
+app.use("/api/usuarios", usersRouter);
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
 });
 
 // Conexión al servidor de la base de datos
 db.authenticate()
-  .then(() => {
-    console.log("Connection has been established successfully.");
-  })
-  .catch((err) => {
-    console.error("Unable to connect to the database:", err);
-  });
+    .then(() => {
+        console.log("Connection has been established successfully.");
+    })
+    .catch((err) => {
+        console.error("Unable to connect to the database:", err);
+    });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
 
 module.exports = app;
--- routes/api/usuarios.js
+++ routes/api/usuarios.js
@@ -3,8 +3,14 @@ const router = express.Router();
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
-router.post("/", (req, res) => {
+router.post('/', (req, res, next) => {
+    let username = req.body.username;
+    let password = req.body.password;
+    let firstName = req.body.firstName;
+    let lastName = req.body.lastName;
+    console.log(lastName);
 
+    res.send({status: 'SUCCESS'});
 });
 
 module.exports = router;
