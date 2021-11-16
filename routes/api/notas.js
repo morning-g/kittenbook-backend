5bcb8c0ee8d58d783da11c09471cd9188e3a5d7a
cattybog <72874739+cattybog@users.noreply.github.com>
2021-11-15 20:00:48 -0600

    Backend notas

--- routes/api/notas.js
+++ routes/api/notas.js
@@ -9,12 +9,77 @@ let auth = function (req, res, next) {
         return res.sendStatus(401);
 };
 
-router.post('/', (req, res, next) => {
-
+router.post('/', auth, (req, res, next) => {
+    if (req.body.titulo === "" || req.body.contenido === "") {
+        return res.status(401).send({
+            message: "No se llenaron los campos requeridos.."
+        });
+    }
+    const nota = {
+        nombre_usuario: req.session.username,
+        titulo: req.body.titulo,
+        contenido: req.body.contenido
+    }
+    database.models.notas.create(nota).then(data => {
+        console.log(data);
+        res.status(200).send();
+    }).catch(err => {
+        console.log(err);
+        res.status(400).send();
+    });
 });
 
-router.get('/', (req, res, next) => {
+router.get('/', auth, (req, res, next) => {
+    database.models.notas.findAll({
+        raw: true,
+        attributes: ['id_nota', 'titulo', 'contenido'],
+        where: {nombre_usuario: req.session.username}
+    }).then(data => {
+        if (data === null) {
+            return res.status(401).send({
+                message: "Notas no encontradas."
+            });
+        }
+        console.log(data);
+        res.status(200).json(data);
+    }).catch(err => {
+        console.log(err);
+        res.status(400).send();
+    });
+});
 
+router.delete('/', auth, (req, res, next) => {
+    database.models.notas.destroy({
+        where: {id_nota: req.body.id_nota}
+    }).then(data => {
+        if (data === null) {
+            return res.status(401).send({
+                message: "Nota no encontrada."
+            });
+        }
+        console.log(data);
+        res.status(200).send();
+    }).catch(err => {
+        console.log(err);
+        res.status(400).send();
+    });
+});
+
+router.post('/actualizar', auth, (req, res, next) => {
+    database.models.notas.update({titulo: req.body.titulo, contenido: req.body.contenido}, {
+        where: {id_nota: req.body.id_nota}
+    }).then(data => {
+        if (data === null) {
+            return res.status(401).send({
+                message: "Nota no encontrada."
+            });
+        }
+        console.log(data);
+        res.status(200).send();
+    }).catch(err => {
+        console.log(err);
+        res.status(400).send();
+    });
 });
 
 module.exports = router;
