88d56be02576a418386cae5df0311eb96c18ea64
cattybog <72874739+cattybog@users.noreply.github.com>
2021-11-29 13:51:37 -0600

    DB prod

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(function (req, res, next) {
     next();
 });
 const corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: '*',
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     optionSuccessStatus: 200
